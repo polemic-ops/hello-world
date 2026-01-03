08b0ee38d40d8f535348bc71d770cf198f2965b8
polemic-ops <dimacheban2026@gmail.com>
2026-01-03 15:50:44 +0200

    adding come documentation

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
-package main
+package main // Defines the entry point. It tells Go that this file should compile as an
+// executable program, not a shared library.
 
-import "fmt"
+import "fmt" // Includes a library. Short for "format". It provides functions
+// formatting text and printing it to the console.
 
+// The starting function. This is where the program execution begins.
 func main() {
+	// Outputs text. It prints the string to the screen and adds a new line at the end.
 	fmt.Println("Hello, World!")
 }
